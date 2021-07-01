c9a66fa689245eb026eeb7c813d32640fdc470c2
Pyro(고정완) <rhwjddhks@gmail.com>
2021-07-02 07:39:02 +0900

    fix: Fix typo

--- backend/revive.sh
+++ backend/revive.sh
@@ -1,7 +1,7 @@
 if [ -z `lsof -t -i tcp:8080` ]
 then
     echo "@@@@ Tomcat is Dead @@@@"
-    java -jar ~/issuetracker.jar >> spring-log.txt 2>&1 &
+    java -jar ~/issuetracker.jar >> ~/spring-log.txt 2>&1 &
 else
     echo "@@@@ Tomcat is Alive @@@@"
 fi

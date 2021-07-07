1314a1ea0c9eaf17244262d892804cd6da1440d0
sanhee <68936@naver.com>
2021-07-07 11:36:48 +0900

    Revert "feat: Label 수정시 Color 필드 글자 수 제한 예외 처리"
    This reverts commit 42ea4969fdbb3f81ab2119b240844d62f40781c5.

--- backend/server/src/main/java/com/issuetracker/service/LabelService.java
+++ backend/server/src/main/java/com/issuetracker/service/LabelService.java
@@ -3,7 +3,6 @@ package com.issuetracker.service;
 import com.issuetracker.dto.request.LabelRequest;
 import com.issuetracker.dto.response.LabelResponse;
 import com.issuetracker.dto.response.LabelsResponse;
-import com.issuetracker.exception.MaxLengthException;
 import com.issuetracker.repository.LabelRepository;
 import org.springframework.stereotype.Service;
 
@@ -24,9 +23,6 @@ public class LabelService {
     }
 
     public void save(LabelRequest labelRequest) {
-        if (labelRequest.getBackgroundColor().length() > 8 || labelRequest.getTextColor().length() > 8) {
-            throw new MaxLengthException("Hex color codes 길이를 초과했습니다. (#000000 ~ #FFFFFF)");
-        }
         labelRepository.save(labelRequest.toLabel());
     }
 

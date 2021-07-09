2600e21ad750c1482e1f3603f92e323da3b529ed
sanhee <68936@naver.com>
2021-07-09 18:09:07 +0900

    feat: 닫기 API 구현

--- backend/server/src/main/java/com/issuetracker/dto/request/IssuesNumbersRequest.java
+++ backend/server/src/main/java/com/issuetracker/dto/request/IssuesNumbersRequest.java
@@ -1,5 +1,6 @@
 package com.issuetracker.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.issuetracker.domain.IssuesNumbers;
 
 import java.util.List;
@@ -7,7 +8,12 @@ import java.util.List;
 //TODO. 네이밍 변경해야함.
 // 클라이언트가 복수개로 선택한 이슈 번호를 서버로 리퀘스트 보낼 때 매칭되는 dto
 public class IssuesNumbersRequest {
-    private final List<Long> issueNumbers;
+
+    @JsonProperty("issueNumbers")
+    private List<Long> issueNumbers;
+
+    public IssuesNumbersRequest() {
+    }
 
     public IssuesNumbersRequest(List<Long> issueNumbers) {
         this.issueNumbers = issueNumbers;

7bee28f13a95b61357930e695d731b8e7fc4147e
Pyro <rhwjddhks@gmail.com>
2021-12-02 17:47:42 +0900

    fix: labesl 와 assignees 가 제대로 보이지 않던 버그 해결

--- frontend/issue-cracker/src/components/layout/IssueDetail/index.tsx
+++ frontend/issue-cracker/src/components/layout/IssueDetail/index.tsx
@@ -4,7 +4,7 @@ import { useSetRecoilState } from 'recoil';
 import styled from 'styled-components';
 import IssueDetailHeader from './IssueDetailHeader';
 import IssueDetailBox from './IssueDetailBox';
-import { issueDetailState, issueEditInputState } from '../../../store/Recoil';
+import { issueDetailState, issueEditInputState, dropCheckState } from '../../../store/Recoil';
 import { Line as S } from '../../styles/CommonStyles';
 import { URL as U } from '../../../utils/const';
 
@@ -12,6 +12,7 @@ const IssueDetail = (): JSX.Element => {
   const location = useLocation<{ id: number }>();
   const setIssueDetail = useSetRecoilState(issueDetailState);
   const setIssueEditInput = useSetRecoilState(issueEditInputState);
+  const setCheckState = useSetRecoilState(dropCheckState);
 
   useEffect(() => {
     fetch(`${U.ISSUES}/${location.state.id}`)
@@ -21,6 +22,11 @@ const IssueDetail = (): JSX.Element => {
       setIssueEditInput({
         title: issueDetail?.title,
         comment: issueDetail?.content,
+      });
+      setCheckState({
+        assignee: issueDetail.assignees,
+        label: issueDetail.labels,
+        milestone: [issueDetail.milestoneInfo]
       });
     });
   });
